d3c23b333725c0ac43e153b1317fa3892abcc79f
Jonatan Missora <jonatanjmissora@gmail.com>
2025-05-15 12:51:48 -0300

    changin URL_domain for deploy

--- app/services/http.services.ts
+++ app/services/http.services.ts
@@ -1,5 +1,5 @@
-const URL_domain = "http://localhost:3000"
-// const URL_domain = "https://digitalmoneyhouse.netlify.app"
+// const URL_domain = "http://localhost:3000"
+const URL_domain = "https://nextjs-dmh-final-demo.vercel.app"
 
 export const httpPost = async (endpoint: string, data?: object | null, token?: string) => {
   const res = await fetch(`${URL_domain}/${endpoint}`, {
